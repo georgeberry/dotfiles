24b66dabeaa5128033552612e426c8199af3a472
George Berry <geb97@cornell.edu>
2016-08-19 10:51:51 -0700

    basic scientific computing stuff

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@ cp .bash_profile ~/.bash_profile
 
 source ~/.bashrc
 source ~/.bash_profile
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/science
+
+brew install R
+brew install python3
+
+pip3 install numpy scipy scikit-learn pandas nltk
