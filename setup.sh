171395f82a69bad0c9ff10c6d40b214e8569fb14
George Berry <geb97@cornell.edu>
2016-09-15 14:53:38 -0400

    added screen rc

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 cp .bashrc ~/.bashrc
 cp .bash_profile ~/.bash_profile
+cp .screenrc ~/.screenrc
 
 source ~/.bashrc
 source ~/.bash_profile
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/science
-
-brew install R
-brew install python3
-
-pip3 install numpy scipy scikit-learn pandas nltk
+source ~/.screenrc
